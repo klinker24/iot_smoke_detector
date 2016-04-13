e36358ac4a5f16dce8e032a6187faf996a053242
Jake Klinker <jklinker1@gmail.com>
2016-04-13 16:15:45 -0500

    accept a message parameter when posting to /auth

--- auth_server/auth.js
+++ auth_server/auth.js
@@ -1,17 +1,16 @@
-//Lets require/import the HTTP module
+// require/import the HTTP module
 var http = require('http');
 var dispatcher = require('httpdispatcher');
 
+// define an RSA public/private key to use
 var NodeRSA = require('node-rsa');
 var key = new NodeRSA({b: 1024});
-console.log(key.exportKey("pkcs8-private"));
-console.log(key.exportKey("pkcs8-public-pem"));
 
-//Lets define a port we want to listen to
+// defines the port we want to listen on
 const PORT=8889; 
 
-//We need a function which handles requests and send response
-function handleRequest(request, response){
+// handles requests and send response
+function handleRequest(request, response) {
     try {
     	console.log(request.url);
     	dispatcher.dispatch(request, response);
@@ -26,15 +25,15 @@ dispatcher.onGet("/auth", function(req, res) {
 });
 
 dispatcher.onPost("/auth", function(req, res) {
+	var encryptedMessage = req.params['message'];
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.end(key.exportKey("pkcs8-private"));
 });
 
-//Create a server
+// create a server
 var server = http.createServer(handleRequest);
 
-//Lets start our server
+// start our server
 server.listen(PORT, function() {
-    //Callback triggered when server is successfully listening. Hurray!
     console.log("Server listening on: http://localhost:%s", PORT);
 });
