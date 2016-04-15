1f33df57b79aa9091ffee9599471bb8a25354e50
Jake Klinker <jklinker1@gmail.com>
2016-04-15 13:22:08 -0500

    when registering in android app, connect to pi and send it auth token

--- android/src/main/java/edu/uiowa/engineering/iot_smoke/util/RegistrationUtils.java
+++ android/src/main/java/edu/uiowa/engineering/iot_smoke/util/RegistrationUtils.java
@@ -5,17 +5,31 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.util.Base64;
 
 import java.io.IOException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.List;
+
+import javax.crypto.Cipher;
 
 import edu.uiowa.engineering.iot_smoke.registration.Registration;
 import edu.uiowa.engineering.iot_smoke.registration.model.AccountRecord;
 import edu.uiowa.engineering.iot_smoke.registration.model.CollectionResponseAccountRecord;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class RegistrationUtils extends BaseUtils {
 
@@ -34,8 +48,46 @@ public class RegistrationUtils extends BaseUtils {
                             .getItems()
                             .get(0);
 
-                    // todo: do something with this account, that contains the auth token
-                } catch (IOException e) {
+                    PreferenceManager.getDefaultSharedPreferences(context).edit()
+                            .putString("pref_auth_token", account.getAuthToken())
+                            .commit();
+
+                    log(account.getAuthToken());
+
+                    // TODO create better way to grab this address (dialog box or something?)
+                    String piIpAddress = "192.168.1.133";
+                    String url = "http://" + piIpAddress + ":8889/auth";
+
+                    OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder()
+                            .url(url)
+                            .method("GET", null)
+                            .build();
+                    Response response = client.newCall(request).execute();
+
+                    String publicKey = response.body().string();
+                    publicKey = publicKey.replaceAll("(-+BEGIN PUBLIC KEY-+\\r?\\n|-+END PUBLIC KEY-+\\r?\\n?)", "");
+                    byte[] keyBytes = Base64.decodeBase64(publicKey);
+                    X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
+                    KeyFactory kf = KeyFactory.getInstance("RSA");
+                    PublicKey pk = kf.generatePublic(spec);
+
+                    byte[] cipherText;
+                    Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA1AndMGF1Padding");
+                    cipher.init(Cipher.ENCRYPT_MODE, pk);
+                    cipherText = cipher.doFinal(account.getAuthToken().getBytes());
+                    String encodedText = Base64.encodeBase64String(cipherText);
+
+                    url = url + "?message=" + encodedText;
+
+                    request = new Request.Builder()
+                            .url(url)
+                            .method("POST", RequestBody.create(MediaType.parse("application/json"),
+                                    "{\"message\":\"" + encodedText + "\"}"))
+                            .header("Content-Length", "0")
+                            .build();
+                    client.newCall(request).execute();
+                } catch (Exception e) {
                     logError(e);
                 }
 
--- auth_server/auth.js
+++ auth_server/auth.js
@@ -12,7 +12,6 @@ const PORT=8889;
 // handles requests and send response
 function handleRequest(request, response) {
     try {
-    	console.log(request.url);
     	dispatcher.dispatch(request, response);
     } catch(err) {
     	console.log(err);
@@ -25,16 +24,17 @@ dispatcher.onGet("/auth", function(req, res) {
 });
 
 dispatcher.onPost("/auth", function(req, res) {
-	var encryptedMessage = req.params['message'].split(' ').join('+');
+	var encryptedMessage = JSON.parse(req.body).message;
 	var decryptedMessage = key.decrypt(encryptedMessage, 'utf-8');
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.end("");
 
-	// TODO do something with the decrypted message, which will contain a 
-	//      user id and an auth token, separated by a comma. I think it
-	//      would work well to set these as environment variables to easily
-	//	be accessed from our other code that takes readings of air
-	//      quality.
+	console.log("Found auth token: " + decryptedMessage);
+
+    var fs = require('fs');
+    fs.writeFile("auth.txt", decryptedMessage, { flag: 'w' }, function (err) {
+        
+    });
 });
 
 // create a server
