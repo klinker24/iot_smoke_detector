8a6bdbb455d49a3e3dcd28d45bec621bf62dcadd
Luke Klinker <lklinker1@gmail.com>
2016-04-19 18:32:50 -0500

    added initial ardiuno code to read the values from the dust sensor

---
+++ arduino/DustSensor.ino
@@ -0,0 +1,27 @@
+int dustPin = 0;
+float dustVal = 0;
+
+int ledPower = 2;
+int delayTime = 280;
+int delayTime2 = 40;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(ledPower,OUTPUT);
+  pinMode(A0, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(ledPower,LOW);
+  delayMicroseconds(delayTime);
+  dustVal= analogRead(dustPin);
+  delayMicroseconds(delayTime2);
+  digitalWrite(ledPower,HIGH);
+  delayMicroseconds(5000);
+
+  delay(3000);
+  Serial.println(dustVal);
+}
