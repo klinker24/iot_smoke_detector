aa04dab5efc44179b4782d5b27c402f4a816268b
Jake Klinker <jklinker1@gmail.com>
2016-04-20 15:37:09 -0500

    update with working arduino code

--- arduino/DustSensor.ino
+++ arduino/DustSensor.ino
@@ -1,27 +1,137 @@
-int dustPin = 0;
-float dustVal = 0;
+/*********************************************************************************************************
+*
+* File                : DustSensor
+* Hardware Environment: 
+* Build Environment   : Arduino
+* Version             : V1.0.5-r2
+* By                  : WaveShare
+*
+*                                  (c) Copyright 2005-2011, WaveShare
+*                                       http://www.waveshare.net
+*                                       http://www.waveshare.com   
+*                                          All Rights Reserved
+*
+*********************************************************************************************************/
+#define        COV_RATIO                       0.2            //ug/mmm / mv
+#define        NO_DUST_VOLTAGE                 400            //mv
+#define        SYS_VOLTAGE                     5000           
 
-int ledPower = 2;
-int delayTime = 280;
-int delayTime2 = 40;
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(ledPower,OUTPUT);
-  pinMode(A0, OUTPUT);
+/*
+I/O define
+*/
+const int iled = 2;                                            //drive the led of sensor
+const int vout = 0;                                            //analog input
 
+/*
+variable
+*/
+float density, voltage;
+int   adcvalue;
+
+/*
+private function
+*/
+int Filter(int m)
+{
+  static int flag_first = 0, _buff[10], sum;
+  const int _buff_max = 10;
+  int i;
+  
+  if(flag_first == 0)
+  {
+    flag_first = 1;
+
+    for(i = 0, sum = 0; i < _buff_max; i++)
+    {
+      _buff[i] = m;
+      sum += _buff[i];
+    }
+    return m;
+  }
+  else
+  {
+    sum -= _buff[0];
+    for(i = 0; i < (_buff_max - 1); i++)
+    {
+      _buff[i] = _buff[i + 1];
+    }
+    _buff[9] = m;
+    sum += _buff[9];
+    
+    i = sum / 10.0;
+    return i;
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(ledPower,LOW);
-  delayMicroseconds(delayTime);
-  dustVal= analogRead(dustPin);
-  delayMicroseconds(delayTime2);
-  digitalWrite(ledPower,HIGH);
-  delayMicroseconds(5000);
 
-  delay(3000);
-  Serial.println(dustVal);
+void setup(void)
+{
+  pinMode(iled, OUTPUT);
+  digitalWrite(iled, LOW);                                     //iled default closed
+  
+  Serial.begin(9600);                                         //send and receive at 9600 baud
+  Serial.print("*********************************** WaveShare ***********************************\n");
 }
+
+void loop(void)
+{
+  /*
+  get adcvalue
+  */
+  digitalWrite(iled, HIGH);
+  delayMicroseconds(280);
+  adcvalue = analogRead(vout);
+  digitalWrite(iled, LOW);
+  
+  adcvalue = Filter(adcvalue);
+  
+  /*
+  covert voltage (mv)
+  */
+  voltage = (SYS_VOLTAGE / 1024.0) * adcvalue * 11;
+  
+  /*
+  voltage to density
+  */
+  if(voltage >= NO_DUST_VOLTAGE)
+  {
+    voltage -= NO_DUST_VOLTAGE;
+    
+    density = voltage * COV_RATIO;
+  }
+  else
+    density = 0;
+    
+  /*
+  display the result
+  */
+  Serial.print("The current dust concentration is: ");
+  Serial.print(density);
+  Serial.print(" ug/m3\n");  
+  
+  delay(1000);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
