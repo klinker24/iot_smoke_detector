8224af3a6f80ade3e67d8ed6f2e0e9f9ce9c78f3
Jake Klinker <jklinker1@gmail.com>
2016-04-21 15:56:45 -0500

    add support for bluetooth to the node.js app

--- auth_server/auth.js
+++ auth_server/auth.js
@@ -5,6 +5,12 @@ var dispatcher = require('httpdispatcher');
 // file system manipulation
 var fs = require('fs');
 
+// bluetooth module
+var noble = require('noble');
+
+// helps to send post requests to server
+var request = require('request');
+
 // define an RSA public/private key to use
 var NodeRSA = require('node-rsa');
 var key = new NodeRSA({b: 1024});
@@ -31,11 +37,13 @@ function handleRequest(request, response) {
     }
 }
 
+// when we receive a get request at /auth
 dispatcher.onGet("/auth", function(req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.end(key.exportKey("pkcs8-public-pem"));
 });
 
+// when we receive a post request to /auth
 dispatcher.onPost("/auth", function(req, res) {
 	var encryptedMessage = JSON.parse(req.body).message;
 	var decryptedMessage = key.decrypt(encryptedMessage, 'utf-8');
@@ -54,4 +62,56 @@ var server = http.createServer(handleRequest);
 // start our server
 server.listen(PORT, function() {
     console.log("Server listening on: http://localhost:%s", PORT);
+});
+
+// start searching for bluetooth devices
+noble.on('stateChange', function(state) {
+  if(state === 'poweredOn') {
+    console.log('Powered on bluetooth!');
+    noble.startScanning();
+  }
+});
+
+// discover bluetooth devices that we can get data from
+noble.on('discover', function(device) {
+
+  // TODO change this to the correct mac address
+  if (device.address === 'e3:0e:74:88:38:5e') {
+    console.log('Found device: ' + device.address);
+
+    // found our device, now connect to it
+    noble.stopScanning();
+
+    device.connect(function(error) {
+
+      // Once connected, we need to kick off service discovery
+      device.discoverAllServicesAndCharacteristics(function(error, services, characteristics) {
+
+        // discovery done! Find characteristics we care about
+        var uartRx = null;
+        characteristics.forEach(function(ch, chID) {
+          if (ch.uuid === '6e400003b5a3f393e0a9e50e24dcca9e') {
+            uartRx = ch;
+          }
+        });
+
+        // check if we found UART Rx characteristic
+        if (!uartRx) {
+          console.log('Failed to find UART Rx Characteristic! ');
+          process.exit();
+        }
+
+        // wait to get data from the arduino and upload it to server where we can parse it
+        uartRx.notify(true);
+        uartRx.on('read', function(data, isNotification) {
+          var url = "https://uiowa-iot-smoke.appspot.com/_ah/api/airQuality/v1/collectionresponse_airqualityrecord/";
+          url = url + encodeURIComponent(data.toString());
+          url = url + "/";
+          url = url + encodeURIComponent(authToken);
+
+          request.post(url, { form: {} }, function (error, response, body) {});
+        });
+      });
+    });
+  }
 });
